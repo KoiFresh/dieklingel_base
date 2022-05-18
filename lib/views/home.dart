03cb15bf992c439ca2e6c035b9ac7a795633bb54
Kai Mayer <kai.mayer@t-online.de>
2022-05-18 18:41:44 +0200

    sign builder added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,11 +1,13 @@
-import 'package:dieklingel_base/views/components/sign.dart';
+import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import '../views/components/sign.dart';
 import '../rtc/rtc_client.dart';
 import '../signaling/signaling_client.dart';
 import '../signaling/signaling_client_mqtt_web.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-
 import '../media/media_ressource.dart';
 import '../signaling/signaling_message.dart';
 import '../signaling/signaling_message_type.dart';
@@ -20,6 +22,7 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   final SignalingClient _signalingClient = SignalingClientMqtt();
   final MediaRessource _mediaResource = MediaRessource();
+  final List<dynamic> _signs = List.empty(growable: true);
   RtcClient? _rtcClient;
   RTCVideoRenderer renderer = RTCVideoRenderer();
 
@@ -51,6 +54,13 @@ class _Home extends State<Home> {
     _rtcClient?.addEventListener(
         RtcClient.mediaReceived, (track) => {renderer.srcObject = track});
     super.initState();
+
+    rootBundle.loadString("config/config.json").then((value) {
+      final dynamic config = jsonDecode(value);
+      setState(() {
+        _signs.addAll(config["signs"]);
+      });
+    });
   }
 
   void onMessageReceived(SignalingMessage message) async {
@@ -65,7 +75,12 @@ class _Home extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ListView.builder(
+        itemCount: _signs.length,
+        itemBuilder: ((context, index) {
+          return Sign(_signs[index]["text"]);
+        }));
+    /*ListView(
       children: const [
         /*CupertinoButton(
           child: const Text("Hallo welt2"),
@@ -83,6 +98,6 @@ class _Home extends State<Home> {
         Sign("Kai\nKai", height: 50),
         Sign("Kai"),
       ],
-    );
+    ); */
   }
 }
