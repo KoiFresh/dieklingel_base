d41dd1880b30be6ef8387494c0ebc84c2d25f949
Kai Mayer <kai.mayer@t-online.de>
2022-06-16 21:33:25 +0200

    fixed: names of the message attributes

--- lib/signaling/signaling_message.dart
+++ lib/signaling/signaling_message.dart
@@ -1,22 +1,22 @@
 import './signaling_message_type.dart';
 
 class SignalingMessage {
-  String from = '';
-  String to = '';
+  String sender = '';
+  String recipient = '';
   SignalingMessageType type = SignalingMessageType.error;
   Map<String, dynamic> data = {};
 
   SignalingMessage();
 
   SignalingMessage.fromJson(Map<String, dynamic> json)
-      : from = json['sender'],
-        to = json['recipient'],
+      : sender = json['sender'],
+        recipient = json['recipient'],
         type = SignalingMessageType.fromString(json['type']),
         data = json['data'];
 
   Map<String, dynamic> toJson() => {
-        'sender': from,
-        'recipient': to,
+        'sender': sender,
+        'recipient': recipient,
         'type': type.toString(),
         'data': data,
       };
