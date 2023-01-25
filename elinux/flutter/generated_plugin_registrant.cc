b7efe322cefd0dbd18fa9a81eeecf92548a324bb
Kai Mayer <kai.mayer@t-online.de>
2023-01-25 16:02:13 +0000

    feat: add flutter-elinux platform

---
+++ elinux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+// Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_elinux/audioplayers_elinux_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersElinuxPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersElinuxPlugin"));
+}
